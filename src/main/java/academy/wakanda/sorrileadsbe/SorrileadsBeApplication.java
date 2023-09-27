3ac543391c9977d661d7eee2b7bf58603774e092
Vinicius Reis <vinicius.reis@msn.com>
2023-09-27 16:39:42 -0300

    spring initializer version

---
+++ src/main/java/academy/wakanda/sorrileadsbe/SorrileadsBeApplication.java
@@ -0,0 +1,13 @@
+package academy.wakanda.sorrileadsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorrileadsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorrileadsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/wakanda/sorrileadsbe/SorrileadsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.wakanda.sorrileadsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorrileadsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
