c7cf7c34d84e7258c244d85f57f2d68290339dda
Alex-Victor <alexvictoribeiro@gmail.com>
2023-10-05 13:11:40 -0300

    implementa messagClientIntegrator

--- src/main/java/academy/wakanda/sorrileadsbe/message/infra/MessageClientIntegrator.java
+++ src/main/java/academy/wakanda/sorrileadsbe/message/infra/MessageClientIntegrator.java
@@ -1,5 +1,19 @@
 package academy.wakanda.sorrileadsbe.message.infra;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import academy.wakanda.sorrileadsbe.message.application.api.MessageRequest;
+import academy.wakanda.sorrileadsbe.message.domain.MessageIntegrator;
+
+@FeignClient(name = "messageInfraClient", url = "https://api.z-api.io/instances/SUA_INSTANCIA/token/SEU_TOKEN/send-text")
 public interface MessageClientIntegrator {
+
+	@PostMapping("send-text")
+	MessageIntegrator sendMessage(@RequestParam("suaInstancia") String instancia,
+								  @RequestParam("seuToken") String token,
+								  @RequestBody MessageRequest messageRequest);
 
 }
--- src/main/java/academy/wakanda/sorrileadsbe/message/infra/MessageInfraIntegrator.java
+++ src/main/java/academy/wakanda/sorrileadsbe/message/infra/MessageInfraIntegrator.java
@@ -1,13 +1,28 @@
 package academy.wakanda.sorrileadsbe.message.infra;
 
+import org.springframework.stereotype.Service;
+
 import academy.wakanda.sorrileadsbe.message.application.api.MessageRequest;
 import academy.wakanda.sorrileadsbe.message.domain.MessageIntegrator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
+@Service
+@RequiredArgsConstructor
 public class MessageInfraIntegrator implements MessageSendIntegrator {
 
+	private final MessageClientIntegrator messageClientIntegrator;
+	
 	@Override
 	public MessageIntegrator sendMessage(MessageRequest messageRequest) {
+		log.info("[inicia] MessageInfraIntegrator - sendMessage");
+		MessageIntegrator messageIntegrator = messageClientIntegrator.sendMessage(INSTANCIA,TOKEN,messageRequest);
+		log.info("[finaliza] MessageInfraIntegrator - sendMessage");
 		return null;
 	}
+
+	private static final String INSTANCIA = null;
+	private static final String TOKEN = null;
 
 }
