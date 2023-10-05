4ab16c605b6d733124a99b2b732f4df62be84047
Alex-Victor <alexvictoribeiro@gmail.com>
2023-10-05 12:04:38 -0300

    implementa service teste ok

--- src/main/java/academy/wakanda/sorrileadsbe/message/application/api/MessageIntegratorController.java
+++ src/main/java/academy/wakanda/sorrileadsbe/message/application/api/MessageIntegratorController.java
@@ -4,18 +4,24 @@ import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import academy.wakanda.sorrileadsbe.message.application.service.MessageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @RestController
+@RequiredArgsConstructor
 public class MessageIntegratorController implements MessageIntegratorAPI {
+
+	private final MessageService messageService;
 
 	@Override
 	public MessageIntegratorResponse sendMessage(@Valid MessageRequest messageRequest) {
 		log.info("[inicia] MessageIntegratorController - sendMessage");
 		log.info("[finaliza] MessageIntegratorController - sendMessage");
+		MessageIntegratorResponse messageIntegrator = messageService.sendMessage(messageRequest);
 		log.info("[messageRequest] {}", messageRequest);
-		return null;
+		return messageIntegrator;
 	}
 
 }
