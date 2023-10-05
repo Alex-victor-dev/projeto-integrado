81d324c5019f20b57dc3903701329492ffed6480
VastiDev <vastiane1@gmail.com>
2023-10-04 21:00:14 -0300

    muda password no yml

---
+++ src/main/java/academy/wakanda/sorrileadsbe/application/service/DuplicatePhoneException.java
@@ -0,0 +1,7 @@
+package academy.wakanda.sorrileadsbe.application.service;
+
+public class DuplicatePhoneException extends Throwable {
+    public DuplicatePhoneException(String message) {
+        super(message);
+    }
+}
