4d98d0f33772c361d2353807e8fae3b7ae3b2360
Vinicius Reis <vinicius.reis@msn.com>
2023-10-09 12:53:46 -0300

    add api de teste e db teste

--- src/main/java/academy/wakanda/sorrileadsbe/SorrileadsBeApplication.java
+++ src/main/java/academy/wakanda/sorrileadsbe/SorrileadsBeApplication.java
@@ -1,10 +1,23 @@
 package academy.wakanda.sorrileadsbe;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@Log4j2
 public class SorrileadsBeApplication {
+
+	@GetMapping("/")
+	public String testHome() {
+		log.info("[start] SorrileadsBeApplication - testHome");
+		String home = "home sorrileads";
+		log.info("[finish] SorrileadsBeApplication - testHome");
+		return home;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SorrileadsBeApplication.class, args);
