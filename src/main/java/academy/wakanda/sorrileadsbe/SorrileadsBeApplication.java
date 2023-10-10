53b0abc62d0256f1938f177429b4074fde44b2fa
Lucas Aquino <lucasaquino350@gmail.com>
2023-10-10 18:42:18 -0300

    remove import

--- src/main/java/academy/wakanda/sorrileadsbe/SorrileadsBeApplication.java
+++ src/main/java/academy/wakanda/sorrileadsbe/SorrileadsBeApplication.java
@@ -4,7 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import org.springframework.web.bind.annotation.RestController;
 
