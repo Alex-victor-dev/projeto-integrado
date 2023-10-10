fec73f4608c04984e82cebcbe9e2ca99a86ddc9a
Alex-Victor <alexvictoribeiro@gmail.com>
2023-10-09 23:38:07 -0300

    implementa tratamento de excecoes na integração do zapi

--- src/main/java/academy/wakanda/sorrileadsbe/communication/infra/CommunicationInfraIntegrator.java
+++ src/main/java/academy/wakanda/sorrileadsbe/communication/infra/CommunicationInfraIntegrator.java
@@ -1,8 +1,11 @@
 package academy.wakanda.sorrileadsbe.communication.infra;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import academy.wakanda.sorrileadsbe.communication.application.api.MessageRequest;
+import academy.wakanda.sorrileadsbe.handler.APIException;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -16,9 +19,21 @@ public class CommunicationInfraIntegrator implements CommunicationIntegrator {
 	@Override
 	public MessageResponse sendMessage(MessageRequest messageRequest) {
 		log.info("[inicia] CommunicationInfraIntegrator - sendMessage");
-		MessageResponse messageResponse = zapiClientIntegrator.sendMessage(messageRequest);
-		log.info("[messageResponse] {}", messageResponse);
-		log.info("[finaliza] CommunicationInfraIntegrator - sendMessage");
-		return messageResponse;
+		try {
+			MessageResponse messageResponse = zapiClientIntegrator.sendMessage(messageRequest);
+			log.info("[messageResponse] {}", messageResponse);
+			return messageResponse;
+			
+		} catch (FeignException feignException) {
+			log.error("Erro ao chamar a API externa com FeignException", feignException);
+			throw APIException.build(HttpStatus.BAD_REQUEST, "Falha Na Integração com a API DO ZAPI");
+		
+		} catch (Exception e) {
+			log.error("Ocorreu uma exceção não tratada", e);
+			throw APIException.build(HttpStatus.INTERNAL_SERVER_ERROR, "Ocorreu uma exceção Não Tratada");
+			
+		} finally {
+			log.info("[finaliza] CommunicationInfraIntegrator - sendMessage");
+		}
 	}
 }
