b1a4622ce428ae1e99251d09b38c007e0c54f754
Lucas Aquino <lucasaquino350@gmail.com>
2023-10-11 19:30:19 -0300

    remove @AllArgsConstructor

--- src/main/java/academy/wakanda/sorrileadsbe/domain/RespondentFormJson.java
+++ src/main/java/academy/wakanda/sorrileadsbe/domain/RespondentFormJson.java
@@ -1,9 +1,7 @@
 package academy.wakanda.sorrileadsbe.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Value;
-@AllArgsConstructor
 @Value
 public class RespondentFormJson {
     private String date;
