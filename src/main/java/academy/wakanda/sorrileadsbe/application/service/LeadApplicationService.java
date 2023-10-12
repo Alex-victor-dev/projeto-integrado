577255343eaf32d3dcd64e791a51b13988b14803
Alex-Victor <alexvictoribeiro@gmail.com>
2023-10-11 21:49:23 -0300

    implementa metodo para enviar mensagen, automatico na captura do lead

--- src/main/java/academy/wakanda/sorrileadsbe/application/service/LeadApplicationService.java
+++ src/main/java/academy/wakanda/sorrileadsbe/application/service/LeadApplicationService.java
@@ -1,14 +1,16 @@
 package academy.wakanda.sorrileadsbe.application.service;
 
-import academy.wakanda.sorrileadsbe.application.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import academy.wakanda.sorrileadsbe.application.api.LeadRequest;
+import academy.wakanda.sorrileadsbe.application.api.LeadResponse;
 import academy.wakanda.sorrileadsbe.application.repository.LeadRepository;
+import academy.wakanda.sorrileadsbe.communication.application.api.MessageRequest;
 import academy.wakanda.sorrileadsbe.communication.application.service.CommunicationService;
-import academy.wakanda.sorrileadsbe.communication.infra.MessageResponse;
 import academy.wakanda.sorrileadsbe.domain.Lead;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
@@ -22,7 +24,8 @@ public class LeadApplicationService implements LeadService {
 	public LeadResponse createLead(LeadRequest leadRequest) {
 		log.info("[start]  LeadApplicationService- createLead");
 		Lead lead = leadRepository.save(new Lead(leadRequest));
-		
+		MessageRequest messageRequest = new MessageRequest(leadRequest);
+		lead.enviaMensagem(communicationService, messageRequest);
 		log.info("[finish]  LeadApplicationService - createLead");
 		return LeadResponse.builder().idLead(lead.getIdLead()).build();
 	}
--- src/main/java/academy/wakanda/sorrileadsbe/domain/Lead.java
+++ src/main/java/academy/wakanda/sorrileadsbe/domain/Lead.java
@@ -1,12 +1,26 @@
 package academy.wakanda.sorrileadsbe.domain;
 
-import academy.wakanda.sorrileadsbe.application.api.LeadRequest;
-import lombok.*;
-import javax.persistence.*;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.util.UUID;
+
+import academy.wakanda.sorrileadsbe.application.api.LeadRequest;
+import academy.wakanda.sorrileadsbe.communication.application.api.MessageRequest;
+import academy.wakanda.sorrileadsbe.communication.application.service.CommunicationService;
+import academy.wakanda.sorrileadsbe.communication.infra.MessageResponse;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
@@ -15,29 +29,42 @@ import java.util.UUID;
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Lead {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", updatable = false, unique = true, nullable = false)
-    private UUID idLead;
-    @NotBlank
-    private String name;
-    @NotBlank
-    @Column(unique = true)
-    private String phone;
-    @Email
-    private String email;
-    @NotNull
-    private MultipleChoice multipleChoice;
-    private String text;
-    private String registrationDate;
-    private boolean resultado;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idLead;
+	@NotBlank
+	private String name;
+	@NotBlank
+	@Column(unique = true)
+	private String phone;
+	@Email
+	private String email;
+	@NotNull
+	private MultipleChoice multipleChoice;
+	private String text;
+	private String registrationDate;
+	private boolean resultado;
 
-    public Lead(LeadRequest leadRequest) {
-        this.name = leadRequest.getRespondentForm().getAnswersJson().getNome();
-        this.phone = leadRequest.getRespondentForm().getAnswersJson().getWhatsapp();
-        this.email = leadRequest.getRespondentForm().getAnswersJson().getEmail();
-        this.multipleChoice = MultipleChoice.fromString(leadRequest.getRespondentForm().getAnswersJson().getEspecialidade());
-        this.text = leadRequest.getRespondentForm().getAnswersJson().getPerguntaEspecifica();
-        this.registrationDate = leadRequest.getRespondentForm().getDate();
-    }
+	public Lead(LeadRequest leadRequest) {
+		this.name = leadRequest.getRespondentForm().getAnswersJson().getNome();
+		this.phone = leadRequest.getRespondentForm().getAnswersJson().getWhatsapp();
+		this.email = leadRequest.getRespondentForm().getAnswersJson().getEmail();
+		this.multipleChoice = MultipleChoice
+				.fromString(leadRequest.getRespondentForm().getAnswersJson().getEspecialidade());
+		this.text = leadRequest.getRespondentForm().getAnswersJson().getPerguntaEspecifica();
+		this.registrationDate = leadRequest.getRespondentForm().getDate();
+	}
+
+	public void enviaMensagem(CommunicationService communicationService, MessageRequest messageRequest) {
+		MessageResponse response = communicationService.sendMessage(messageRequest);
+
+		if (response != null && response.getZaapId() != null && response.getMessageId() != null
+				&& response.getId() != null) {
+			this.resultado = true;
+		} else {
+			this.resultado = false;
+		}
+	}
+
 }
