b994daae4f362f0de46897f4214c32f2aec87c3e
Vinicius Reis <vinicius.reis@msn.com>
2023-10-18 18:37:22 -0300

    remove unsed classes

--- src/main/java/academy/wakanda/sorrileadsbe/testedb/TesteDbRepository.java
+++
@@ -1,8 +0,0 @@
-package academy.wakanda.sorrileadsbe.testedb;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface TesteDbRepository extends JpaRepository<TesteDb, UUID> {
-}
