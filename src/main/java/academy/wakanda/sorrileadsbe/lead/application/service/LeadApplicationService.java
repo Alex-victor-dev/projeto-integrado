92ca87d0e793503fcf43b015d2767158724ac94e
Alex-Victor <alexvictoribeiro@gmail.com>
2023-11-21 16:19:08 -0300

    add construct, add metodo converte uma lista de leads em DTO

--- src/main/java/academy/wakanda/sorrileadsbe/lead/application/service/LeadApplicationService.java
+++ src/main/java/academy/wakanda/sorrileadsbe/lead/application/service/LeadApplicationService.java
@@ -1,13 +1,20 @@
 package academy.wakanda.sorrileadsbe.lead.application.service;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import academy.wakanda.sorrileadsbe.clinic.application.service.ClinicRepository;
+import academy.wakanda.sorrileadsbe.clinic.domain.Clinic;
 import academy.wakanda.sorrileadsbe.communication.application.service.CommunicationService;
+import academy.wakanda.sorrileadsbe.lead.application.api.LeadListResponse;
 import academy.wakanda.sorrileadsbe.lead.application.api.LeadRequest;
 import academy.wakanda.sorrileadsbe.lead.application.api.LeadResponse;
 import academy.wakanda.sorrileadsbe.lead.application.repository.LeadRepository;
 import academy.wakanda.sorrileadsbe.lead.domain.Lead;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
@@ -16,6 +23,7 @@ public class LeadApplicationService implements LeadService {
 
 	private final LeadRepository leadRepository;
 	private final CommunicationService communicationService;
+	private ClinicRepository clinicRepository;
 
 	@Override
 	public LeadResponse createLead(LeadRequest leadRequest) {
@@ -25,5 +33,12 @@ public class LeadApplicationService implements LeadService {
 		log.info("[finish]  LeadApplicationService - createLead");
 		return new LeadResponse(lead);
 
+	}
+
+	@Override
+	public List<LeadListResponse> getLeadsByClinicUrl(UUID idClinic) {
+		clinicRepository.buscaClinicPerId(idClinic);
+		List<Lead> leadsList = leadRepository.getLeadsByClinicUrl(idClinic);
+		return LeadListResponse.converte(leadsList);
 	}
 }
