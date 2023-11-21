1922b59aaca102544ec068e3cd3a20d64bf711e4
Alex-Victor <alexvictoribeiro@gmail.com>
2023-11-21 16:09:16 -0300

    implementa controller

--- src/main/java/academy/wakanda/sorrileadsbe/lead/application/api/DashboardController.java
+++ src/main/java/academy/wakanda/sorrileadsbe/lead/application/api/DashboardController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import academy.wakanda.sorrileadsbe.lead.application.service.LeadService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -13,11 +14,14 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class DashboardController implements DashboardAPI {
 
+	private LeadService leadService;
+
 	@Override
 	public List<LeadListResponse> getLeadsByClinicUrl(UUID idClinic) {
 		log.info("[start]  DashboardController - getLeadsByClinicUrl");
+		List<LeadListResponse> leads = leadService.getLeadsByClinicUrl(idClinic);
 		log.info("[finish]  DashboardController - getLeadsByClinicUrl");
-		return null;
+		return leads;
 	}
 
 }
--- src/main/java/academy/wakanda/sorrileadsbe/lead/application/service/LeadService.java
+++ src/main/java/academy/wakanda/sorrileadsbe/lead/application/service/LeadService.java
@@ -1,9 +1,14 @@
 package academy.wakanda.sorrileadsbe.lead.application.service;
 
-import academy.wakanda.sorrileadsbe.lead.application.api.*;
+import java.util.List;
+import java.util.UUID;
+
+import academy.wakanda.sorrileadsbe.lead.application.api.LeadListResponse;
 import academy.wakanda.sorrileadsbe.lead.application.api.LeadRequest;
 import academy.wakanda.sorrileadsbe.lead.application.api.LeadResponse;
 
 public interface LeadService {
     LeadResponse createLead(LeadRequest leadRequest);
+
+	List<LeadListResponse> getLeadsByClinicUrl(UUID idClinic);
 }
