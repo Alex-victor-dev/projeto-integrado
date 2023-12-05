6055b2ad022d00a7c706b9bb2e402e32d89834c8
rodrigomarques2002 <rodrigomqs550@gmail.com>
2023-12-05 16:11:10 -0300

    adiciona link de deploy do front

---
+++ src/main/java/academy/wakanda/sorrileadsbe/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package academy.wakanda.sorrileadsbe.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://656f731525a57c2699e0c098--cerulean-tiramisu-e6bd18.netlify.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
